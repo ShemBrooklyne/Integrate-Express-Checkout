27372a421171bdead7e4b3b0ceebd8a856ebd3d7
sbmwangi <shem.mwangi@cellulant.io>
2022-04-15 21:23:57 +0300

    initial commit

---
+++ src/main/java/com/geekwys/expresscheckout/ExpressCheckoutApplication.java
@@ -0,0 +1,13 @@
+package com.geekwys.expresscheckout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpressCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpressCheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geekwys/expresscheckout/ExpresscheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geekwys.expresscheckout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpresscheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
